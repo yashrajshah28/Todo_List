6d0ded7d11406207251427cc7159846de5238773
Yashraj Shah <yashrajshah28@gmail.com>
2022-09-17 01:34:59 +0530

    Initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-dark text-light py-3">
+            <p className="text-center">
+            Copyright &copy; Made with ❤ by Freak
+            </p>
+        </footer>
+    )
+}
